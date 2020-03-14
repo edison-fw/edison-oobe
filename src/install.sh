2173d285b5c193439df5cb824edb73295ac40e32
Matthias Dieter Wallnöfer <mwallnoefer@users.noreply.github.com>
2020-03-14 14:57:53 +0100

    install.sh - remove dead code

--- src/install.sh
+++ src/install.sh
@@ -2,7 +2,6 @@
 
 mkdir -p /usr/lib/edison_config_tools
 cp -r public /usr/lib/edison_config_tools
-cp server.js /usr/lib/edison_config_tools/edison-config-server.js
 
 cp edison_config.service /lib/systemd/system
 systemctl daemon-reload
@@ -10,9 +9,6 @@ systemctl enable edison_config
 
 cp configure_edison /usr/bin
 chmod a+x /usr/bin/configure_edison
-
-#cp start_edison_config_service /usr/bin
-#chmod a+x /usr/bin/start_edison_config_service
 
 echo ""
 echo ""
