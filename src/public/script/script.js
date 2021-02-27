ae3bd3f894094371931b014cafcb07e2f00b87aa
Matthias Dieter Wallnöfer <mdw@samba.org>
2021-02-27 12:24:07 +0100

    script.js - cleanup (variable declarations, drop unused code)

--- src/public/script/script.js
+++ src/public/script/script.js
@@ -1,16 +1,13 @@
-
 'use strict';
 
-window.onload = init; 
-
-//window.onunload = exit;
+window.onload = init;
 
 // CONST
-var MAX_CONTENT_WIDTH = 600;
+const MAX_CONTENT_WIDTH = 600;
 
 // GLOBAL VARS
-var _bannerTitle;
-var _initDone = false;
+let _bannerTitle;
+let _initDone = false;
 
 // initialization routine
 function init() {
@@ -18,49 +15,36 @@ function init() {
   // so we might be doing this twice
   if(_initDone)
     return;
-    
+
   //alert(">>> init");
   _initDone = true;
   setBannerTitle(_bannerTitle);
-
-  // fix stuff
-  //exalon_onresize();
 }
- 
+
 // set the title of the banner
 function setBannerTitle(title) {
-  var titleElement = getObject("bannertitle");
+  const titleElement = document.getElementById("bannertitle");
   if(titleElement!=null)
     titleElement.innerHTML = title;
 }
 
 /*
 function exalon_onresize() {
-  var content = getObject("content");
+  const content = getElementById("content");
   if(content==null)
     return;
-    
+
   with(content) {
     if(offsetWidth > MAX_CONTENT_WIDTH)
       style.width = MAX_CONTENT_WIDTH;
   }
-};
+}
 window.attachEvent("onresize", exalon_onresize);
 */
 
-function getObject(name) {
-  if(document.getElementById)
-    return document.getElementById(name);
-    
-  if(document.all)
-    return document.all[name];
-
-  return null;
-}
-
 function toggleVisibility(collapsedDiv, expandedDiv) {
-  var collapsed = document.getElementById(collapsedDiv);
-  var expanded = document.getElementById(expandedDiv);
+  const collapsed = document.getElementById(collapsedDiv);
+  const expanded = document.getElementById(expandedDiv);
   if(collapsed.style.display == "block") {
     collapsed.style.display = "none";
     expanded.style.display = "block";
@@ -71,6 +55,6 @@ function toggleVisibility(collapsedDiv, expandedDiv) {
 }
 
 function toggleDiv(id) {
-    var div = document.getElementById(id);
-    div.style.display = div.style.display == "none" ? "block" : "none";
+  const div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
 }
